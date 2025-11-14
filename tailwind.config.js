1666760d1dba8d491d86da03b36f0130cf4c8f86
makej7403-bit <makej7403@gmail.com>
2025-11-14 09:40:43 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0b74ff",
+        accent: "#f5b700"
+      }
+    }
+  },
+  plugins: [],
+};
